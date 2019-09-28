007238b219a2a1bac679128d108a717e92ca1d70
Michael S Muegel <mike@muegel.org>
2019-09-28 15:43:38 -0500

    Skip adding cells with null views: keeps setup code simple

--- Sources/Grid.iOS/Layout/Grid.Layout.cs
+++ Sources/Grid.iOS/Layout/Grid.Layout.cs
@@ -440,6 +440,12 @@ namespace GridView
 
 			public static Layout operator +(Layout layout, Cell cell)
 			{
+                if (cell.View == null)
+                {
+                    LogLine($"Skipping {cell}: null view");
+                    return layout;
+                }
+                
                 // "Stack" is supported through automatic creation of 
                 // necessary grid rows/columns
                 if (cell.Position.StackType == StackType.Horizontal)
